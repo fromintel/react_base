8d6f300e2322a1e8631fc7b21442300ffa8a1e0a
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-17 20:20:28 +0200

    0.1 [react-base] Create a new separate component - PostsForm

---
+++ src/components/PostForm.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import CoreInput from "./UI/CoreInput/CoreInput";
+import CoreButton from "./UI/CoreButton/CoreButton";
+
+const PostForm = ({create}) => {
+    const [post, setPost] = useState({ title: '', desc: '' });
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        const newPost = { ...post, id: Date.now() };
+        create(newPost);
+        setPost({ title: '', desc: '' });
+    }
+
+    return (
+        <form>
+            <fieldset>
+                <label htmlFor="postName">Post name</label>
+                <CoreInput
+                    type="text"
+                    id={'postName'}
+                    onChange={e => setPost({ ...post, title: e.target.value })}
+                    placeholder={'Enter the name of post...'}
+                    value={post.title}
+                />
+            </fieldset>
+            <fieldset>
+                <label htmlFor="desc">Post description</label>
+                <CoreInput
+                    name="description"
+                    id="desc"
+                    onChange={e => setPost({ ...post, desc: e.target.value })}
+                    placeholder={'Enter the description...'}
+                    value={post.desc}
+                />
+            </fieldset>
+            <CoreButton onClick={addNewPost}>test</CoreButton>
+        </form>
+    );
+};
+
+export default PostForm;
