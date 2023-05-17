c957a13e088799f62569a7593e7701c5bb5bba34
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-17 20:54:07 +0200

    0.1 [react-base] Create a logic to sort posts

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import CoreSelect from "./components/UI/CoreSelect/CoreSelect";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: 'Post 1', desc: 'Post 1 description'},
-        {id: 2, title: 'Post 2', desc: 'Post 2 description'},
-        {id: 3, title: 'Post 3', desc: 'Post 3 description'},
+        {id: 1, title: 'aaa', desc: 'w'},
+        {id: 2, title: 'vvv', desc: 'a'},
+        {id: 3, title: 'bbb', desc: 'l'},
     ])
+
+    const [selectedSort, setSelectedSort] = useState('')
 
     const createPost = (newPost) => {
         setPosts([ ...posts, newPost ])
@@ -20,17 +23,36 @@ function App() {
         setPosts(posts.filter((p) => post.id !== p.id));
     }
 
+    const sortPosts = (sortValue) => {
+        setSelectedSort(sortValue);
+        setPosts([...posts].sort((a, b) => a[sortValue].localeCompare(b[sortValue])))
+    }
+
     return (
         <div className="App">
             <div className="counters">
                 <Counter/>
                 <ClassCounter/>
             </div>
+            <hr/>
             <PostForm create={createPost}/>
-            { posts.length
-                ? <PostList remove={removePost} posts={posts} title={'Post List 1'}/>
-                : <div>Any post is not found...</div>
-            }
+            <section className={'posts-list'}>
+                <div className={'posts-list__sort'}>
+                    <CoreSelect
+                        value={selectedSort}
+                        onChange={sortPosts}
+                        defaultOption='Sort by'
+                        options={[
+                            { title: 'By name', value: 'title' },
+                            { title: 'By desc', value: 'desc' },
+                        ]}
+                    />
+                </div>
+                { posts.length
+                    ? <PostList remove={removePost} posts={posts} title={'Post List 1'}/>
+                    : <div>Any post is not found...</div>
+                }
+            </section>
 
         </div>
     );
