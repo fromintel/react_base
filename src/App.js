1c3f674ca1f3d6df680ba7110bc9e1fded9854bb
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-18 21:23:42 +0200

    0.1 [react-base] Refactoring of App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './styles/App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './styles/App.css';
-import About from './pages/About';
-import Posts from './pages/Posts';
-import Counters from "./pages/Counters";
+import CoreNavbar from './components/UI/CoreNavbar/CoreNavbar';
+import AppRouter from './components/AppRouter';
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/about" element={<About />} />
-                <Route path="/posts" element={<Posts />} />
-                <Route path="/counters" element={<Counters />} />
-            </Routes>
+            <CoreNavbar/>
+            <div className="App">
+                <AppRouter/>
+            </div>
         </BrowserRouter>
     )
 }
