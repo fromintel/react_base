bc1785a825dac494e39520e1e1b4fb18caaf9b21
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-18 22:52:47 +0200

    0.1 [react-base] Use useContext in root of project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles/App.css';
 import {BrowserRouter} from 'react-router-dom';
 import './styles/App.css';
 import CoreNavbar from './components/UI/CoreNavbar/CoreNavbar';
 import AppRouter from './components/AppRouter';
+import {AuthContext} from './context';
 
 function App() {
+    const [isAuth, setIsAuth] = useState(false);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (localStorage.getItem('auth')) {
+            setIsAuth(true);
+        }
+        setLoading(false);
+    }, []);
+
     return (
-        <BrowserRouter>
-            <CoreNavbar/>
-            <div className="App">
-                <AppRouter/>
-            </div>
-        </BrowserRouter>
+        <AuthContext.Provider value={{
+            isAuth,
+            setIsAuth,
+            isLoading,
+        }}>
+            <BrowserRouter>
+                <CoreNavbar/>
+                <div className="App">
+                    <AppRouter/>
+                </div>
+            </BrowserRouter>
+        </AuthContext.Provider>
     )
 }
 export default App;
