518e9a9fdc493049f8d7f89d6ba598fb33d9397c
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-18 21:24:46 +0200

    0.1 [react-base] Create a post details component

---
+++ src/components/PostDetailsItem.jsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {useFetching} from '../hooks/useFetching';
+import PostsService from '../api/PostsService';
+import CoreLoader from './UI/CoreLoader/CoreLoader';
+
+const PostDetailsItem = () => {
+    const params = useParams();
+    const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
+    const [fetchPostId, isPostLoading] = useFetching(async (id) => {
+        const response = await PostsService.getById(id);
+        setPost(response.data)
+    })
+    const [fetchComments, isCommentsLoading] = useFetching(async (id) => {
+        const response = await PostsService.getCommentsByPostId(id);
+        setComments(response.data)
+    })
+
+    useEffect(() => {
+        fetchPostId(params.id);
+        fetchComments(params.id)
+    }, [])
+
+    return (
+        <div>
+            <h2 style={{textAlign: 'center'}}>{post.id} - {post.title}</h2>
+            {
+                isPostLoading
+                    ? <CoreLoader/>
+                    : <div>{post.body}</div>
+            }
+            <section className='comments'>
+                <h3>Comments</h3>
+                {
+                    isCommentsLoading
+                        ? <CoreLoader/>
+                        : <div className='comments__container'>
+                            { comments.map((comment) => <div style={{ marginTop: '20px' }}>
+                                <h5>{comment.email}</h5>
+                                <div>{comment.body}</div>
+                            </div>) }
+                        </div>
+                }
+            </section>
+        </div>
+    );
+};
+
+export default PostDetailsItem;
