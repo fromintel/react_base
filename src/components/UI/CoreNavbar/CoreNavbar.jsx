050e66d89fee33d765ec09b747a8fe6bc22faa2a
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-18 22:53:22 +0200

    0.1 [react-base] Create logout functionality

--- src/components/UI/CoreNavbar/CoreNavbar.jsx
+++ src/components/UI/CoreNavbar/CoreNavbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
+import CoreButton from '../CoreButton/CoreButton';
+import {AuthContext} from '../../../context';
 
 const CoreNavbar = () => {
+    const {isAuth, setIsAuth} = useContext(AuthContext);
+
+    const logout = () => {
+        setIsAuth(false);
+        localStorage.removeItem('auth');
+    }
+
     return (
         <nav className='navbar'>
+            {
+                isAuth && <CoreButton onClick={logout}>Logout</CoreButton>
+            }
             <div className='navbar__items'>
                 <Link to="/about">About</Link>
                 <Link to="/posts">Posts</Link>
