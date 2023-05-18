854e0f952f42a2d2f78a53deb9d5e2c0270a2fc8
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-18 21:24:18 +0200

    0.1 [react-base] Create an error component

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h2 style={{ color: 'red' }}>You have came in undefined page...</h2>
+        </div>
+    );
+};
+
+export default Error;
