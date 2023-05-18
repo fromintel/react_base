cdf561bcf95e200243412316c1c0f185b766913c
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-18 08:13:07 +0200

    0.1 [react-base] Create a new custom loader component

---
+++ src/components/UI/CoreLoader/CoreLoader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './CoreLoader.module.css'
+
+const CoreLoader = () => {
+    return (
+        <div className={classes['core-loader']}>
+
+        </div>
+    );
+};
+
+export default CoreLoader;
